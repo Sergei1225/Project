577b2594a46efa6679c2e0690326592da9136459
Sergei <igertsin@inbox.ru>
2021-11-30 19:45:47 +0500

    first 560t

--- index2.js
+++ index2.js
@@ -39,3 +39,109 @@ console.log(janeOne);
 function hoh () {
     alert('hello');
 }
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+"use strict";
+
+let jane = ' fucking world';
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+"use strict";
+
+let jane = ' fucking world';
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+"use strict";
+
+let jane = ' fucking world';
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+"use strict";
+
+let jane = ' fucking world';
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+"use strict";
+
+let jane = ' fucking world';
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+"use strict";
+
+let jane = ' fucking world';
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+"use strict";
+
+let jane = ' fucking world';
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+"use strict";
+
+let jane = ' fucking world';
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+"use strict";
+
+let jane = ' fucking world';
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+"use strict";
+
+let jane = ' fucking world';
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+function hoh () {
+    alert('hello');
+}
+
