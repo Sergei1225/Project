8f27fb17145aff725f61015b014d24f67d6a2b9f
Sergei <igertsin@inbox.ru>
2021-11-30 18:48:02 +0500

    first commit2

---
+++ index2.js
@@ -0,0 +1,17 @@
+
+"use strict";
+
+let jane = ' fucking world';
+let janeOne = `Fuckyou${jane}`;
+console.log(janeOne);
+
+var nethbhb = 665656; 
+var nethbhb = 665656; 
+var nethbhb = 665656; 
+var nethbhb = 665656; 
+var nethbhb = 665656; 
+var nethbhb = 665656; 
+var nethbhb = 665656; 
+var nethbhb = 665656; 
+var nethbhb = 665656; 
+var nethbhb = 665656; 
